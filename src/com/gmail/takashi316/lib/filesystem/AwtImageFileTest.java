049c523aa6780af9522f5dfaec35b761928a560c
Takashi SASAKI <takashi316@gmail.com>
2012-06-27 20:11:59 +0900

    AwtImageFileTest for a unit test

---
+++ src/com/gmail/takashi316/lib/filesystem/AwtImageFileTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.gmail.takashi316.lib.filesystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Takashi
+ * 
+ */
+public class AwtImageFileTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.gmail.takashi316.lib.filesystem.AwtImageFile#getSize()}.
+	 */
+	@Test
+	public final void testGetSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.gmail.takashi316.lib.filesystem.AwtImageFile#AwtImageFile(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public final void testAwtImageFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
