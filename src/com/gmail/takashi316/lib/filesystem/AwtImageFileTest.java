5471f66812343b4fcf601f78ca9dd6fd42bae5fa
Takashi SASAKI <takashi316@gmail.com>
2012-06-27 20:17:58 +0900

    AwtImageFileTest passes tests

--- src/com/gmail/takashi316/lib/filesystem/AwtImageFileTest.java
+++ src/com/gmail/takashi316/lib/filesystem/AwtImageFileTest.java
@@ -5,6 +5,8 @@ package com.gmail.takashi316.lib.filesystem;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,10 +14,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * @author Takashi
+ * @author Takashi SASAKI {@link "http://twitter.com/TakashiSasaki"}
  * 
  */
 public class AwtImageFileTest {
+	final Directory currentDirectory = new Directory();
+	ImageFile imageFile;
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,7 +40,10 @@ public class AwtImageFileTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
-	}
+
+		this.imageFile = new AwtImageFile(new File(this.currentDirectory,
+				"readonly/1 ghi 100 200/lena.bmp").toString());
+	}// setUp
 
 	/**
 	 * @throws java.lang.Exception
@@ -47,21 +54,13 @@ public class AwtImageFileTest {
 
 	/**
 	 * Test method for
-	 * {@link com.gmail.takashi316.lib.filesystem.AwtImageFile#getSize()}.
-	 */
-	@Test
-	public final void testGetSize() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for
 	 * {@link com.gmail.takashi316.lib.filesystem.AwtImageFile#AwtImageFile(java.lang.String)}
 	 * .
 	 */
 	@Test
 	public final void testAwtImageFile() {
-		fail("Not yet implemented"); // TODO
-	}
+		assertEquals(this.imageFile.height, 512);
+		assertEquals(this.imageFile.width, 512);
+	}// testAwtImageFile
 
-}
+}// AwtImageFileTest
